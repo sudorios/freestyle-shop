fe430cb6982816cef551992b75f4b6e6896377a0
sudorios <wtke90@gmail.com>
2025-06-24 09:12:07 -0500

    feat: redirect unauthenticated users to index

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (!isset($_SESSION['usuario'])) {
+    header('Location: login.php');
+    exit();
+}
 include_once 'conexion/cone.php';
 include_once 'includes/head.php';
 include_once 'includes/header.php';
